d1f5e96bf4c6ca24954f8c9dd9b2e804490ff26e
JonathanLeal <jonathan.e.leal.m@gmail.com>
2023-06-21 10:23:47 -0600

    update

--- app/Http/Controllers/DetalleFacturaController.php
+++ app/Http/Controllers/DetalleFacturaController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class DetalleFacturaController extends Controller
 {
-    //
+    protected $table = "detallefactura";
+    protected $primaryKey = "codDetalle";
+    public $timestamps = false;
 }
